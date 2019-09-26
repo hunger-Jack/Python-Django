d44219538400c40d40705f0d938ffbdb48f70efd
jack <2959921118@qq.com>
2019-09-27 03:40:47 +0800

    注册管理后台图书类和英雄类，并且自定义自定义管理页面

--- demo/booktest/admin.py
+++ demo/booktest/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from booktest.models import BookInfo,HeroInfo
 
 # Register your models here.
 # 后台管理相关文件-可视化操作数据库的各种操作
+
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ["id", "b_title", "b_pub_date"]
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ["id", "h_name", "h_gender", "h_comment", "h_book_id"]
+
+
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo, HeroInfoAdmin)

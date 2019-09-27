953f3db5e5f22c3215546edc58723f3843b13e8a
jack <2959921118@qq.com>
2019-09-28 05:32:18 +0800

    模板调用

--- demo/booktest/views.py
+++ demo/booktest/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,RequestContext
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("this is index page")
+    """index模板"""
+    # # 1. 获取模板
+    # template = loader.get_template("booktest/index.html")
+    #
+    # # 2. 定义上下文
+    # context = {"title":"图书列表","list":range(15)}
+    #
+    # # 3. 渲染模板
+    # res_html = template.render(context)
+    #
+    # # 4. 返回给浏览器html内容
+    # return HttpResponse(res_html)
+    return render(request, "booktest/index.html", {"title":"图书列表","list":range(15)})
